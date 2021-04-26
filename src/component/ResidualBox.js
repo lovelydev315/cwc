4d6c451739d351bc309654735e4340c959cabfdc
ruslan0420 <leonidgolovin1021@gmail.com>
2021-04-26 13:42:34 -0700

    graph 19

--- src/component/ResidualBox.js
+++ src/component/ResidualBox.js
@@ -79,11 +79,16 @@ export default class ResidualBox extends React.Component{
 
     }
     render() {
-        return <div style={{ width: this.props.width, height: this.props.height, position: "relative"}}>
+        return <div style={{ width: this.props.width, height: this.props.height, position: "relative", paddingLeft: "20px", paddingBottom: "20px", margin: "10px"}}>
             {Object.keys(colorsWithNames).length ? <div style={{position: "absolute", top: "20px", right: "20px", padding: "8px"}}>
                 {Object.keys(colorsWithNames).map(key => <div style={{display: "flex", justifyContent: "flex-start", alignItems: "center"}}><span style={{backgroundColor: colorsWithNames[key], width: "20px", height: "5px", marginRight: "10px"}}></span><p className="m-0 font-weight-bold">{key}</p></div>)}
             </div> : <div></div>}
-          {data.length ? <Chart data={data} axes={axes} series={series} /> : <div style={{width: "100%", display: "flex", justifyContent: "center", margin: "20px"}}><div className="spinner-border text-primary" role="status">
+            {data.length ? <>
+                <div style={{position: "absolute", top: 0, left: 0, alignItems: "center"}}>
+                    <p style={{writingMode: "vertical-lr", textOrientation: "upright"}}>Resiual</p>
+                </div>
+                <Chart data={data} axes={axes} series={series} />
+            </> : <div style={{width: "100%", display: "flex", justifyContent: "center", margin: "20px"}}><div className="spinner-border text-primary" role="status">
               <span className="sr-only">Loading...</span>
              </div></div>
             }
